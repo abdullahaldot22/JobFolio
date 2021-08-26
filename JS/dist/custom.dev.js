b105265a31f9e17a50054ec96661d1f619a063a3
Abdullah AL Ishaq <alishaq912@protonmail.com>
2021-08-27 00:10:29 +0600

    one

---
+++ JS/dist/custom.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+$(window).on("load", function () {
+  $("#load").fadeOut("slow");
+});
+$(document).ready(function () {
+  $(".slide").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 900,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    asNavFor: '.list-item'
+  });
+  $(".list-item").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 900,
+    fade: true,
+    fadeSpeed: 4000,
+    asNavFor: '.slide'
+  });
+});
